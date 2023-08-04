93aeae6925e6465b33922531bedd1bbbc0134396
nideesh <nideeshnd313@gmail.com>
2023-08-04 15:45:25 +0530

    last commit

--- routes/errorroutes.js
+++ routes/errorroutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import the errorController
-const { errorController } = require('../Controllers/errorController');
+const { errorController } = require('../controllers/errorcontroller');
 
 // 404 Error Page
 router.use((req, res, next) => {
